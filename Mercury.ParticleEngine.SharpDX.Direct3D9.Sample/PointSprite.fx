534700b002782b9a15fba4e5715a51152f10aedb
Matt Davey <matthew.davey@wiggle.co.uk>
2013-08-08 13:54:25 +0100

    Added sample project demonstrating DirectX9 PointSprite renderer.

---
+++ Mercury.ParticleEngine.SharpDX.Direct3D9.Sample/PointSprite.fx
@@ -0,0 +1,80 @@
+uniform extern float4x4 WVPMatrix;
+uniform extern texture SpriteTexture;
+
+struct VS_INPUT
+{
+	float Age : TEXCOORD1;
+	float4 Position : POSITION0;
+	float4 Color : COLOR0;
+	//float Size : PSIZE;
+	//float Rotation : COLOR1;
+};
+
+struct VS_OUTPUT
+{
+	float4 Position : POSITION0;
+	float4 Color : COLOR0;
+	float Size : PSIZE;
+	float Rotation : COLOR1;
+};
+
+struct PS_INPUT
+{
+	float4 Color : COLOR0;
+	float Rotation : COLOR1;
+    float2 TexCoord : TEXCOORD0;
+};
+
+sampler Sampler = sampler_state
+{
+    Texture = <SpriteTexture>;
+    
+    MinFilter = LINEAR;
+    MagFilter = LINEAR;
+    MipFilter = POINT;
+    
+    AddressU = CLAMP;
+    AddressV = CLAMP;
+};                        
+
+VS_OUTPUT vshader(VS_INPUT input)
+{
+	VS_OUTPUT output = (VS_OUTPUT)0;
+	
+	output.Position = mul(input.Position, WVPMatrix);
+	output.Color = input.Color;
+	//output.Size = input.Size;
+	output.Size = 32.0f;
+	//output.Rotation = input.Rotation;
+	output.Rotation = 0.0f;
+
+	return output;
+}
+
+float4 pshader(PS_INPUT input) : COLOR0
+{
+	float2 texCoord;
+	
+	float2 cCoord = input.TexCoord;
+	
+	cCoord += 0.5f;
+	
+	float ca = cos(input.Rotation);
+	float sa = sin(input.Rotation);
+	
+	texCoord.x = cCoord.x * ca - cCoord.y * sa;
+	texCoord.y = cCoord.x * sa + cCoord.y * ca;
+	
+	texCoord -= 0.5f;
+
+    return tex2D(Sampler, texCoord) * input.Color;
+}
+
+technique PointSprite_2_0
+{
+    pass P0
+    {
+        vertexShader = compile vs_3_0 vshader();
+        pixelShader = compile ps_3_0 pshader();
+    }
+}
---
+++ Mercury.ParticleEngine.SharpDX.Direct3D9.Sample/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Mercury.ParticleEngine
+{
+    using System;
+    using SharpDX;
+    using SharpDX.Direct3D9;
+    using SharpDX.Windows;
+    using Mercury.ParticleEngine.Profiles;
+    using Mercury.ParticleEngine.Renderers;
+
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            var form = new RenderForm("Mercury Particle Engine - SharpDX.Direct3D9 Sample");
+
+            var direct3d = new Direct3D();
+            var device = new Device(direct3d, 0, DeviceType.Hardware, form.Handle, CreateFlags.HardwareVertexProcessing, new PresentParameters(form.ClientSize.Width, form.ClientSize.Height));
+
+            var view = new Matrix(
+                1.0f, 0.0f, 0.0f, 0.0f,
+                0.0f, -1.0f, 0.0f, 0.0f,
+                0.0f, 0.0f, -1.0f, 0.0f,
+                0.0f, 0.0f, 0.0f, 1.0f);
+            var proj = Matrix.OrthoOffCenterLH(form.ClientSize.Width * -0.5f, form.ClientSize.Width * 0.5f, form.ClientSize.Height * 0.5f, form.ClientSize.Height * -0.5f, 0f, 100f);
+
+            var emitter = new Emitter(5000, 3.0f, Profile.Point())
+            {
+                Parameters = new ReleaseParameters
+                {
+                    Colour = new ColourRange(new RangeF(0f, 0.3f), new RangeF(0.3f, 0.7f), new RangeF(0.8f, 1f)),
+                    Opacity = new RangeF(1f, 1f),
+                    Quantity = new Range(25, 25),
+                    Speed = new RangeF(-1f, 1f)
+                }
+            };
+            var renderer = new PointSpriteRenderer(device, emitter);
+
+            var texture = Texture.FromFile(device, "Particle.dds");
+
+            RenderLoop.Run(form, () =>
+                {
+                    emitter.Trigger(0f, 0f);
+                    emitter.Update(0.01666666f);
+
+// ReSharper disable AccessToDisposedClosure
+                    device.Clear(ClearFlags.Target | ClearFlags.ZBuffer, Color.Black, 1f, 0);
+                    device.BeginScene();
+
+                    renderer.Render(Matrix.Identity * view * proj, texture);
+
+                    device.EndScene();
+                    device.Present();
+// ReSharper restore AccessToDisposedClosure
+
+                });
+
+            device.Dispose();
+            direct3d.Dispose();
+        }
+    }
+}
