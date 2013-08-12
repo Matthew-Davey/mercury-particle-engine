58272e6ecdcd3897a3cf2cc8a2e3279139f59395
Matt Davey <romeblue@gmail.com>
2013-08-12 21:51:34 +0100

    Added DampingModifier.

---
+++ Mercury.ParticleEngine.Core/Modifiers/DampingModifier.cs
@@ -0,0 +1,20 @@
+﻿namespace Mercury.ParticleEngine.Modifiers
+{
+    public class DampingModifier : Modifier
+    {
+        public float DampingCoefficient;
+
+        protected internal unsafe override void Update(float elapsedSeconds, ref ParticleIterator iterator)
+        {
+            var particle = iterator.First;
+            var deltaDampingCoefficient = 1f - (DampingCoefficient * elapsedSeconds);
+
+            do
+            {
+                particle->Velocity[0] *= deltaDampingCoefficient;
+                particle->Velocity[1] *= deltaDampingCoefficient;
+            }
+            while (iterator.MoveNext(&particle));
+        }
+    }
+}
