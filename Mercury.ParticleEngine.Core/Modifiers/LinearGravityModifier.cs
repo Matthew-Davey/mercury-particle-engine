31ddbc4d57a86daf5820a3b3f2df0dc9d5f21f57
Matt Davey <romeblue@gmail.com>
2013-08-13 20:31:55 +0100

    Added LinearGravityModifier.

---
+++ Mercury.ParticleEngine.Core/Modifiers/LinearGravityModifier.cs
@@ -0,0 +1,28 @@
+﻿namespace Mercury.ParticleEngine.Modifiers
+{
+    public class LinearGravityModifier : Modifier
+    {
+        public LinearGravityModifier(Axis direction, float strength)
+        {
+            Direction = direction;
+            Strength = strength;
+        }
+
+        public Axis Direction { get; set; }
+        public float Strength { get; set; }
+
+        protected internal override unsafe void Update(float elapsedSeconds, ref ParticleIterator iterator)
+        {
+            var deltaStrength = Strength * elapsedSeconds;
+
+            var particle = iterator.First;
+
+            do
+            {
+                particle->Velocity[0] += Direction._x * deltaStrength;
+                particle->Velocity[1] += Direction._y * deltaStrength;
+            }
+            while (iterator.MoveNext(&particle));
+        }
+    }
+}
