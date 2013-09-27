6750b96aaf921d6e174111584d522d57828a948a
Matt Davey <romeblue@gmail.com>
2013-09-27 21:37:42 +0100

    Added hue interpolator modifier.

---
+++ Mercury.ParticleEngine.Core/Modifiers/HueInterpolator2.cs
@@ -0,0 +1,20 @@
+﻿namespace Mercury.ParticleEngine.Modifiers
+{
+    public class HueInterpolator2 : Modifier
+    {
+        public float InitialHue;
+        public float FinalHue;
+
+        protected internal override unsafe void Update(float elapsedSeconds, Particle* particle, int count)
+        {
+            var delta = FinalHue - InitialHue;
+
+            while (count-- > 0)
+            {
+                particle->Colour[0] = (delta * particle->Age) + InitialHue;
+
+                particle++;
+            }
+        }
+    }
+}
