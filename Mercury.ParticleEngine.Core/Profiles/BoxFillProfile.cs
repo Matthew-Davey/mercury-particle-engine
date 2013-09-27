3e9bbf872b2a7321f6a8c52f6afe20bd3cc0049f
Matt Davey <romeblue@gmail.com>
2013-09-27 21:38:35 +0100

    Added box fill profile.

---
+++ Mercury.ParticleEngine.Core/Profiles/BoxFillProfile.cs
@@ -0,0 +1,16 @@
+﻿namespace Mercury.ParticleEngine.Profiles
+{
+    public class BoxFillProfile : Profile
+    {
+        public float Width;
+        public float Height;
+
+        public override unsafe void GetOffsetAndHeading(Coordinate* offset, Axis* heading)
+        {
+            *offset = new Coordinate(FastRand.NextSingle(Width * -0.5f, Width * 0.5f),
+                                     FastRand.NextSingle(Height * -0.5f, Height * 0.5f));
+
+            FastRand.NextUnitVector((Vector*)heading);
+        }
+    }
+}
