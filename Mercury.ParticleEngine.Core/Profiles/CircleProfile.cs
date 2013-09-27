7c346e6211ab6fad400f40d5d59e9b7cdb0e0778
Matt Davey <romeblue@gmail.com>
2013-09-27 21:39:17 +0100

    Added circle profile.

---
+++ Mercury.ParticleEngine.Core/Profiles/CircleProfile.cs
@@ -0,0 +1,20 @@
+﻿namespace Mercury.ParticleEngine.Profiles
+{
+    public class CircleProfile : Profile
+    {
+        public float Radius;
+        public bool Radiate;
+
+        public override unsafe void GetOffsetAndHeading(Coordinate* offset, Axis* heading)
+        {
+            var dist = FastRand.NextSingle(0f, Radius);
+
+            FastRand.NextUnitVector((Vector*)heading);
+
+            *offset = new Coordinate(heading->_x * dist, heading->_y * dist);
+
+            if (!Radiate)
+                FastRand.NextUnitVector((Vector*)heading);
+        }
+    }
+}
