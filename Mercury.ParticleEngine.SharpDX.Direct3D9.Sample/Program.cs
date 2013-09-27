4d70e50860e30b84085020712f171eb089dc561d
Matt Davey <romeblue@gmail.com>
2013-09-27 21:44:23 +0100

    Updated sample application.

--- Mercury.ParticleEngine.SharpDX.Direct3D9.Sample/Program.cs
+++ Mercury.ParticleEngine.SharpDX.Direct3D9.Sample/Program.cs
@@ -11,45 +11,46 @@
     using Mercury.ParticleEngine.Profiles;
     using Mercury.ParticleEngine.Renderers;
     using System.Threading.Tasks;
+    using System.Windows.Input;
 
     static class Program
     {
         [STAThread]
         static void Main()
         {
-            const int width = 1024;
-            const int height = 768;
+            var worldSize = new DrawingPoint(1024, 768);
+            var renderSize = new DrawingPoint(1280, 720);
             const bool windowed = true;
 
-            const int budget = 100000;
+            const int budget = 200000;
 
             const int numEmitters = 5;
 
             var form = new RenderForm("Mercury Particle Engine - SharpDX.Direct3D9 Sample")
             {
-                Size = new System.Drawing.Size(width, height)
+                Size = new System.Drawing.Size(renderSize.X, renderSize.Y)
             };
 
             var direct3d = new Direct3D();
-            var device = new Device(direct3d, 0, DeviceType.Hardware, form.Handle, CreateFlags.HardwareVertexProcessing | CreateFlags.EnablePresentStatistics, new PresentParameters(width, height) { PresentationInterval = PresentInterval.Immediate, Windowed = windowed});
+            var device = new Device(direct3d, 0, DeviceType.Hardware, form.Handle, CreateFlags.HardwareVertexProcessing, new PresentParameters(renderSize.X, renderSize.Y) { PresentationInterval = PresentInterval.Default, Windowed = windowed });
 
             var view = new Matrix(
                 1.0f, 0.0f, 0.0f, 0.0f,
                 0.0f, -1.0f, 0.0f, 0.0f,
                 0.0f, 0.0f, -1.0f, 0.0f,
                 0.0f, 0.0f, 0.0f, 1.0f);
-            var proj = Matrix.OrthoOffCenterLH(width * -0.5f, width * 0.5f, height * 0.5f, height * -0.5f, 0f, 100f);
+            var proj = Matrix.OrthoOffCenterLH(worldSize.X * -0.5f, worldSize.X * 0.5f, worldSize.Y * 0.5f, worldSize.Y * -0.5f, 0f, 1f);
             var wvp = Matrix.Identity * view * proj;
 
             var releaseParameters = new ReleaseParameters
                 {
-                    Colour   = new ColourRange(new RangeF(.5f, 1f), new RangeF(.5f, 1f), new RangeF(.5f, 1f)),
+                    Colour   = new Colour(220f, 0.7f, 0.3f),
                     Opacity  = 1f,
-                    Quantity = 1,
+                    Quantity = budget,
                     Speed    = new RangeF(0f, 200f),
                     Scale    = 1f,
                     Rotation = 0f,
-                    Mass     = 10f
+                    Mass     = new RangeF(8f, 12f)
                 };
 
             var modifiers = new List<Modifier>
@@ -57,31 +58,51 @@
                 new VortexModifier
                 {
                     Position = Coordinate.Origin,
-                    Mass = 500f
+                    Mass     = 200f,
+                    MaxSpeed = 1000f
                 },
                 //new ColourInterpolator2
                 //{
                 //    //InitialColour = new Colour(0f, 0f, 0f),
-                //    InitialColour = new Colour(220f, .5f, .2f),
-                //    FinalColour = new Colour(280f, .5f, .4f)
+                //    InitialColour   = new Colour(280f, .5f, .5f),
+                //    FinalColour     = new Colour(220f, .5f, .5f)
                 //},
-                new VelocityColourModifier
+                //new VelocityColourModifier
+                //{
+                //    StationaryColour  = new Colour(220f, 0.7f, 0.5f),
+                //    VelocityColour    = new Colour(300f, 0.7f, 0.5f),
+                //    VelocityThreshold = 800f
+                //},
+                new VelocityHueModifier
                 {
-                    StationaryColour = new Colour(180f, 0.5f, 0.1f),
-                    VelocityColour = new Colour(0f, 1f, 0.25f),
+                    StationaryHue     = 220f,
+                    VelocityHue       = 300f,
                     VelocityThreshold = 800f
                 },
                 new DragModifier
                 {
-                    DampingCoefficient = 0.5f
-                }
+                    DragCoefficient = .47f,
+                    Density         = .15f
+                },
+                new ContainerModifier
+                {
+                    Position = Coordinate.Origin,
+                    Width    = worldSize.X,
+                    Height   = worldSize.Y
+                },
+                //new HueInterpolator2
+                //{
+                //    InitialHue = 80f,
+                //    FinalHue   = 300f
+                //},
+                //new LinearGravityModifier(Axis.Down, 10f)
             };
 
             var emitters = new Emitter[numEmitters];
 
             for (int i = 0; i < numEmitters; i++)
             {
-                emitters[i] = new Emitter(budget, TimeSpan.FromSeconds(2), Profile.Point())
+                emitters[i] = new Emitter(budget, TimeSpan.FromSeconds(600), Profile.BoxFill(worldSize.X, worldSize.Y))
                 {
                     Parameters = releaseParameters,
                     BlendMode = BlendMode.Add,
@@ -98,10 +119,10 @@
 
             var fontDescription = new FontDescription
             {
-                Height = 16,
-                FaceName = "Consolas",
+                Height         = 16,
+                FaceName       = "Consolas",
                 PitchAndFamily = FontPitchAndFamily.Mono,
-                Quality = FontQuality.Draft
+                Quality        = FontQuality.Draft
             };
 
             var font = new Font(device, fontDescription);
@@ -109,79 +130,73 @@
             var totalTimer = Stopwatch.StartNew();
             var updateTimer = new Stopwatch();
             var renderTimer = new Stopwatch();
-            var adjustmentTimer = Stopwatch.StartNew();
 
             var totalTime = 0f;
 
-            var timings = new List<float>();
+            emitters[0].Trigger(Coordinate.Origin);
+            emitters[1].Trigger(Coordinate.Origin);
+            emitters[2].Trigger(Coordinate.Origin);
+            emitters[3].Trigger(Coordinate.Origin);
+            emitters[4].Trigger(Coordinate.Origin);
 
             RenderLoop.Run(form, () =>
                 {
+                    float updateTime;
+
+                    var renderTask = Task.Factory.StartNew(() =>
+                    {
+                        device.Clear(ClearFlags.Target, Color.Black, 1f, 0);
+                        device.BeginScene();
+
+                        renderTimer.Restart();
+                        for (int i = 0; i < numEmitters; i++)
+                        {
+                            renderer.Render(emitters[i], wvp, texture);
+                        }
+                        renderTimer.Stop();
+                        var renderTime = (float)renderTimer.Elapsed.TotalSeconds;
+
+                        font.DrawText(null, String.Format("Time:        {0}", totalTimer.Elapsed), 0, 0, Color.White);
+                        font.DrawText(null, String.Format("Particles:   {0:n0}", emitters[0].ActiveParticles * numEmitters), 0, 16, Color.White);
+                        //font.DrawText(null, String.Format("Update:      {0:n4} ({1,8:P2})", updateTime, updateTime / 0.01666666f), 0, 32, Color.White);
+                        //font.DrawText(null, String.Format("Render:      {0:n4} ({1,8:P2})", renderTime, renderTime / 0.01666666f), 0, 48, Color.White);
+
+                        device.EndScene();
+                        device.Present();
+                    });
+
                     var frameTime = ((float)totalTimer.Elapsed.TotalSeconds) - totalTime;
                     totalTime = (float)totalTimer.Elapsed.TotalSeconds;
 
-                    modifiers.OfType<VortexModifier>().Single().Position = new Coordinate(
-                        (float)Math.Sin(totalTime * 2f) * (width * 0.1f),
-                        (float)Math.Cos(totalTime * 3f) * (height * 0.1f)
-                    );
+                    var mousePosition = form.PointToClient(RenderForm.MousePosition);
+
+                    Vector3 mouseVector = new Vector3(mousePosition.X, mousePosition.Y, 0f);
+                    var unprojected = Vector3.Unproject(mouseVector, 0, 0, renderSize.X, renderSize.Y, 0f, 1f, wvp);
+
+                    modifiers.OfType<VortexModifier>().First().Position = new Coordinate(unprojected.X, unprojected.Y);
 
                     updateTimer.Restart();
 
-                    for (int i = 0; i < numEmitters; i++)
-                    {
-                        emitters[i].Trigger(new Coordinate((float)Math.Sin(totalTime + i) * (width * 0.4f), (float)Math.Cos(((totalTime * 0.5f) + i ) * 4f) * (height * 0.4f)));
-                    }
+                    //for (int i = 0; i < numEmitters; i++)
+                    //{
+                    //    emitters[i].Trigger(Coordinate.Origin);
+                    //    emitters[i].Trigger(new Coordinate((float)Math.Sin(totalTime + i) * (worldSize.X * 0.4f), (float)Math.Cos(((totalTime * 0.5f) + i) * 4f) * (worldSize.Y * 0.4f)));
+                    //}
 
                     Parallel.ForEach(emitters, emitter => emitter.Update(frameTime));
                     updateTimer.Stop();
-                    var updateTime = (float)updateTimer.Elapsed.TotalSeconds;
+                    updateTime = (float)updateTimer.Elapsed.TotalSeconds;
 
 // ReSharper disable AccessToDisposedClosure
-                    device.Clear(ClearFlags.Target, Color.Black, 1f, 0);
-                    device.BeginScene();
+                    Task.WaitAll(renderTask);
 
-                    renderTimer.Restart();
-                    for (int i = 0; i < numEmitters; i++)
-                    {
-                        renderer.Render(emitters[i], wvp, texture);
-                    }
-                    renderTimer.Stop();
-                    var renderTime = (float)renderTimer.Elapsed.TotalSeconds;
+                    if (Keyboard.IsKeyDown(Key.Escape))
+                        Environment.Exit(0);
 
-                    font.DrawText(null, String.Format("Time:        {0}", totalTimer.Elapsed), 0, 0, Color.White);
-                    font.DrawText(null, String.Format("Particles:   {0:n0}", emitters[0].ActiveParticles * numEmitters), 0, 16, Color.White);
-                    font.DrawText(null, String.Format("Update:      {0:n4} ({1,8:P2})", updateTime, updateTime / 0.01666666f), 0, 32, Color.White);
-                    font.DrawText(null, String.Format("Render:      {0:n4} ({1,8:P2})", renderTime, renderTime / 0.01666666f), 0, 48, Color.White);
-
-                    device.EndScene();
-                    device.Present();
-
-                    timings.Add((updateTime + renderTime) / 0.01666666f);
-
-                    if (adjustmentTimer.ElapsedMilliseconds > 200)
-                    {
-                        var averageTime = timings.Average();
-                        timings.Clear();
-
-                        if (averageTime > 1f)
-                        {
-                            releaseParameters.Quantity = Math.Max(releaseParameters.Quantity.X - 1, 0);
-                        }
-                        else if (averageTime < 0.75f)
-                        {
-                            releaseParameters.Quantity = releaseParameters.Quantity.X + 30;
-                        }
-                        else if (averageTime < 0.85f)
-                        {
-                            releaseParameters.Quantity = releaseParameters.Quantity.X + 10;
-                        }
-                        else if (averageTime < 0.95f)
-                        {
-                            releaseParameters.Quantity = releaseParameters.Quantity.X + 1;
-                        }
-
-                        adjustmentTimer.Restart();
-                    }
+                    if (Keyboard.IsKeyDown(Key.Space))
+                        modifiers.OfType<VortexModifier>().Single().Mass = -200f;
+                    else
+                        modifiers.OfType<VortexModifier>().Single().Mass = 200f;
 // ReSharper restore AccessToDisposedClosure
                 });
 
