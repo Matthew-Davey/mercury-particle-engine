6e3ab9f5c1f1416d9c9dd92295dbc7b3cd66f185
Matt Davey <matthew.davey@wiggle.co.uk>
2013-10-04 10:28:08 +0100

    ContainerModifier now better handles particles which are moving fast, and has a restitution coefficient parameter.

--- Mercury.ParticleEngine.Core/Modifiers/ContainerModifier.cs
+++ Mercury.ParticleEngine.Core/Modifiers/ContainerModifier.cs
@@ -1,40 +1,43 @@
 ﻿namespace Mercury.ParticleEngine.Modifiers
 {
+    using System;
+
     public sealed unsafe class ContainerModifier : Modifier
     {
         public Coordinate Position;
         public int Width;
         public int Height;
+        public float RestitutionCoefficient;
 
         protected internal override void Update(float elapsedSeconds, Particle* particle, int count)
         {
-            var left   = Width  * -0.5f;
-            var right  = Width  * 0.5f;
-            var top    = Height * -0.5f;
+            var left = Width * -0.5f;
+            var right = Width * 0.5f;
+            var top = Height * -0.5f;
             var bottom = Height * 0.5f;
 
             while (count-- > 0)
             {
-                if (particle->Position[0] < left)
+                if ((int)particle->Position[0] < left)
                 {
-                    particle->Position[0] = left;
-                    particle->Velocity[0] = -particle->Velocity[0];
+                    particle->Position[0] = left + (left - particle->Position[0]);
+                    particle->Velocity[0] = -particle->Velocity[0] * RestitutionCoefficient;
                 }
-                else if (particle->Position[0] > right)
+                else if ((int)particle->Position[0] > right)
                 {
-                    particle->Position[0] = right;
-                    particle->Velocity[0] = -particle->Velocity[0];
+                    particle->Position[0] = right - (particle->Position[0] - right);
+                    particle->Velocity[0] = -particle->Velocity[0] * RestitutionCoefficient;
                 }
 
-                if (particle->Position[1] < top)
+                if ((int)particle->Position[1] < top)
                 {
-                    particle->Position[1] = top;
-                    particle->Velocity[1] = -particle->Velocity[1];
+                    particle->Position[1] = top + (top - particle->Position[1]);
+                    particle->Velocity[1] = -particle->Velocity[1] * RestitutionCoefficient;
                 }
-                else if (particle->Position[1] > bottom)
+                else if ((int)particle->Position[1] > bottom)
                 {
-                    particle->Position[1] = bottom;
-                    particle->Velocity[1] = -particle->Velocity[1];
+                    particle->Position[1] = bottom - (particle->Position[1] - bottom);
+                    particle->Velocity[1] = -particle->Velocity[1] * RestitutionCoefficient;
                 }
 
                 particle++;
