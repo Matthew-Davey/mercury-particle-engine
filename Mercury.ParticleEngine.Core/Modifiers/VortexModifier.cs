082ffb782fd00c376c95a4d519cd9bdee52370b0
Matt Davey <matthew.davey@wiggle.co.uk>
2013-10-04 10:29:14 +0100

    Performance enhancement for VortexModifier.

--- Mercury.ParticleEngine.Core/Modifiers/VortexModifier.cs
+++ Mercury.ParticleEngine.Core/Modifiers/VortexModifier.cs
@@ -12,23 +12,21 @@ namespace Mercury.ParticleEngine.Modifiers
         {
             while (count-- > 0)
             {
-                double distx = Position._x - particle->Position[0];
-                double disty = Position._y - particle->Position[1];
+                var distx = Position._x - particle->Position[0];
+                var disty = Position._y - particle->Position[1];
 
-                var distance = Math.Sqrt((distx * distx) + (disty * disty));
+                var distance2 = (distx * distx) + (disty * disty);
+                var distance = (float)Math.Sqrt(distance2);
 
-                var m = (10000d * Mass * particle->Mass) / (distance * distance);
+                var m = (10000f * Mass * particle->Mass) / distance2;
 
-                m = Math.Max(Math.Min(m, MaxSpeed), -MaxSpeed) * elapsedSeconds;
+                m = (float)Math.Max(Math.Min(m, MaxSpeed), -MaxSpeed) * elapsedSeconds;
 
-                distx /= distance;
-                disty /= distance;
+                distx = (distx / distance) * m;
+                disty = (disty / distance) * m;
 
-                distx *= m;
-                disty *= m;
-
-                particle->Velocity[0] += (float)distx;
-                particle->Velocity[1] += (float)disty;
+                particle->Velocity[0] += distx;
+                particle->Velocity[1] += disty;
 
                 particle++;
             }
