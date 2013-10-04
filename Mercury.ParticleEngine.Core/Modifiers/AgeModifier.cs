121da4c88040229f92e3a8150a862a27dadcd55e
Matt Davey <matthew.davey@wiggle.co.uk>
2013-10-04 10:30:27 +0100

    Particle movement and age calculation are now done in modifiers.

---
+++ Mercury.ParticleEngine.Core/Modifiers/AgeModifier.cs
@@ -0,0 +1,27 @@
+﻿namespace Mercury.ParticleEngine.Modifiers
+{
+    using System;
+
+    public class AgeModifier : Modifier
+    {
+        public AgeModifier(TimeSpan term)
+        {
+            _term = (float)term.TotalSeconds;
+        }
+
+        private float _term;
+        private float _totalSeconds;
+
+        protected internal override unsafe void Update(float elapsedSeconds, Particle* particle, int count)
+        {
+            _totalSeconds += elapsedSeconds;
+
+            while (count-- > 0)
+            {
+                particle->Age = (_totalSeconds - particle->Inception) / _term;
+
+                particle++;
+            }
+        }
+    }
+}
---
+++ Mercury.ParticleEngine.Core/Modifiers/MoveModifier.cs
@@ -0,0 +1,16 @@
+﻿namespace Mercury.ParticleEngine.Modifiers
+{
+    public class MoveModifier : Modifier
+    {
+        protected internal override unsafe void Update(float elapsedSeconds, Particle* particle, int count)
+        {
+            while (count-- > 0)
+            {
+                particle->Position[0] += particle->Velocity[0] * elapsedSeconds;
+                particle->Position[1] += particle->Velocity[1] * elapsedSeconds;
+
+                particle++;
+            }
+        }
+    }
+}
