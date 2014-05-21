3127e6cd65685bc42dd2772d38582b7ea6e7b677
Matt Davey <romeblue@gmail.com>
2014-05-21 23:37:55 +0100

    build can now be set to debug or release

--- .build/default.ps1
+++ .build/default.ps1
@@ -1,7 +1,8 @@
 properties {
   $solutionPath  = "..\Mercury.ParticleEngine.proj"
+  $configuration = "Debug"
   $xunitConsole  = ".\xunit.console.clr4.x86.exe"
-  $unitTests     = "..\Mercury.ParticleEngine.Core.Tests\bin\Debug\Mercury.ParticleEngine.Core.Tests.dll"
+  $unitTests     = "..\Mercury.ParticleEngine.Core.Tests\bin\$($configuration)\Mercury.ParticleEngine.Core.Tests.dll"
 }
 
 Import-Module ".\teamcity.psm1"
@@ -20,13 +21,13 @@ task Test -depends Compile {
 
 task Compile -depends Clean {
   Exec {
-    msbuild $solutionPath /t:Build /m /tv:4.0 /p:Configuration=Debug
+    msbuild $solutionPath /t:Build /m /tv:4.0 /p:Configuration=$($configuration)
   }
 }
 
 task Clean {
   Exec {
-    msbuild $solutionPath /t:Clean /m /tv:4.0 /p:Configuration=Debug
+    msbuild $solutionPath /t:Clean /m /tv:4.0 /p:Configuration=$($configuration)
   }
 }
 
